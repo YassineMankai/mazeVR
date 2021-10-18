89e51673136d9a00cdf0d28242fd276eb2235bad
lbertrand417 <81630925+lbertrand417@users.noreply.github.com>
2021-10-18 18:10:19 +0200

    Final changes

---
+++ VR_maze/Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
